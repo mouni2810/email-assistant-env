927453e7f02bc4826c1ddb0f62d41350d724a4a0
Mounika Gummadila <gummadilamounika10@gmail.com>
2026-03-31 19:03:31 +0530

    Fixed server entry point

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import uvicorn
-from app import app
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
+
 if __name__ == "__main__":
     main()
